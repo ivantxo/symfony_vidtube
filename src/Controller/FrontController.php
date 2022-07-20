deae673b880e314215063b4489eac8a9ca189d8c
IvanR <ivan.ramirezf@gmail.com>
2022-07-20 23:54:22 +1000

    More front modifications
    * Added new method on FrontController
    * Renamed videolist to video_list
    * Using {{ path('video_list') }} directive, where video_list is the name of the FrontController function
    * Modified video_list.html.twig
      * To extend 'base.html.twig' and added {% block main %}
      * Set variable sample_videos twig way
      * Using {{ path('video_details') }}
    Modified video_details.html.twig
      * To extend 'base.html.twig' and added {% block main %}
      * Some other changes

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -15,4 +15,20 @@ class FrontController extends AbstractController
     {
         return $this->render('front/index.html.twig');
     }
+
+	/**
+	 * @Route("/video_list", name="video_list")
+	 */
+	public function video_list(): Response
+	{
+		return $this->render('front/video_list.html.twig');
+	}
+
+	/**
+	 * @Route("/video_details", name="video_details")
+	 */
+	public function video_details(): Response
+	{
+		return $this->render('front/video_details.html.twig');
+	}
 }
