1777640912281f9fe0d69f9cea581dbb6c4b959d
IvanR <ivan.ramirezf@gmail.com>
2022-08-19 21:27:41 +1000

    Admin controller and views
    * Added edit_category in AdminController
    * Admin templates now using template mode instead of procedural php

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -50,4 +50,12 @@ class AdminController extends AbstractController
 	{
 		return $this->render('admin/users.html.twig');
 	}
+
+	/**
+	 * @Route("/edit_category", name="edit_category")
+	 */
+	public function edit_category(): Response
+	{
+		return $this->render('admin/edit_category.html.twig');
+	}
 }
