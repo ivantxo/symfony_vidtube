c7b997154627acce1c562f1508663e788df99f44
IvanR <ivan.ramirezf@gmail.com>
2022-08-25 20:51:20 +1000

    Modified fixture to insert subcategories
    * Sub-category creation is more dynamic
    * bin/console doctrine:schema:drop -n -q --force --full-database &&
        rm migrations/*.php &&
        bin/console make:migration &&
        bin/console doctrine:migrations:migrate -n -q &&
        bin/console doctrine:fixtures:load -n -q

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -11,16 +11,32 @@ class CategoryFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
 		$this->loadMainCategories($manager);
+		$this->loadSubCategories($manager, 'Electronics', 1);
     }
 
 	private function loadMainCategories(ObjectManager $manager)
 	{
-		foreach ($this->getMainCategoriesData() as $name)
+		foreach ($this->getMainCategoriesData() as [$name])
 		{
 			$category = new Category();
-			$category->setName('name');
+			$category->setName($name);
 			$manager->persist($category);
 		}
+		$manager->flush();
+	}
+
+	private function loadSubCategories(ObjectManager $manager, $category, $parentId)
+	{
+		$parent = $manager->getRepository(Category::class)->find($parentId);
+		$methodName = "get{$category}Data";
+		foreach ($this->$methodName() as [$name])
+		{
+			$category = new Category();
+			$category->setName($name);
+			$category->setParent($parent);
+			$manager->persist($category);
+		}
+		$manager->flush();
 	}
 
 	private function getMainCategoriesData(): array
@@ -30,6 +46,15 @@ class CategoryFixtures extends Fixture
 			['Toys', 2],
 			['Books', 3],
 			['Movies', 4],
+		];
+	}
+
+	private function getElectronicsData(): array
+	{
+		return [
+			['Cameras', 5],
+			['Computers', 6],
+			['Cell Phones', 7],
 		];
 	}
 }
