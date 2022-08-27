cc6e3a99d007c395c4d756ae0ba3f8c33ef6d203
IvanR <ivan.ramirezf@gmail.com>
2022-08-28 02:26:05 +1000

    Added custom twig extention
    * Added too a function to lowecase the url.
    Added custom twig extention
    
    * Added too a function to lowecase the url.
    Added custom twig extention
    
    * Added too a function to lowecase the url.
    Added custom twig extention
    
    * Added too a function to lowecase the url.
    Added custom twig extention
    
    * Added too a function to lowecase the url.
    Added custom twig extention
    
    * Added too a function to lowecase the url.
    Added custom twig extention
    
    * Added too a function to lowecase the url.
    Added custom twig extention
    
    * Added too a function to lowecase the url.

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class AppExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/3.x/advanced.html#automatic-escaping
+            new TwigFilter('slugify', [$this, 'slugify']),
+        ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('function_name', [$this, 'doSomething']),
+        ];
+    }
+
+    public function slugify($url): string
+    {
+        $url = preg_replace("/ +/", "-", trim($url));
+		return mb_strtolower(preg_replace('/[^A-Za-z0-9-]+/', '', $url));
+    }
+}
