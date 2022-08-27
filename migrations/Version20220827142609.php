5dd8a23900c9ada09a43a28c6a34f2cb7d92be3b
IvanR <ivan.ramirezf@gmail.com>
2022-08-28 00:31:19 +1000

    Fixture is now loading more categories

--- migrations/Version20220827142609.php
+++ migrations/Version20220827142609.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220825104833 extends AbstractMigration
+final class Version20220827142609 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -11,7 +11,12 @@ class CategoryFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
 		$this->loadMainCategories($manager);
-		$this->loadSubCategories($manager, 'Electronics', 1);
+		$this->loadElectronics($manager);
+		$this->loadComputers($manager);
+		$this->loadLaptops($manager);
+		$this->loadBooks($manager);
+		$this->loadMovies($manager);
+		$this->loadRomance($manager);
     }
 
 	private function loadMainCategories(ObjectManager $manager)
@@ -23,6 +28,36 @@ class CategoryFixtures extends Fixture
 			$manager->persist($category);
 		}
 		$manager->flush();
+	}
+
+	private function loadElectronics(ObjectManager $manager)
+	{
+		$this->loadSubCategories($manager, 'Electronics', 1);
+	}
+
+	private function loadComputers(ObjectManager $manager)
+	{
+		$this->loadSubCategories($manager, 'Computers', 6);
+	}
+
+	private function loadLaptops(ObjectManager $manager)
+	{
+		$this->loadSubCategories($manager, 'Laptops', 8);
+	}
+
+	private function loadBooks(ObjectManager $manager)
+	{
+		$this->loadSubCategories($manager, 'Books', 3);
+	}
+
+	private function loadMovies(ObjectManager $manager)
+	{
+		$this->loadSubCategories($manager, 'Movies', 4);
+	}
+
+	private function loadRomance(ObjectManager $manager)
+	{
+		$this->loadSubCategories($manager, 'Romance', 18);
 	}
 
 	private function loadSubCategories(ObjectManager $manager, $category, $parentId)
@@ -57,4 +92,48 @@ class CategoryFixtures extends Fixture
 			['Cell Phones', 7],
 		];
 	}
+
+	private function getComputersData(): array
+	{
+		return [
+			['Laptops', 8],
+			['Desktops', 9],
+		];
+	}
+
+	private function getLaptopsData()
+	{
+		return [
+			['Apple',10],
+			['Asus',11],
+			['Dell',12],
+			['Lenovo',13],
+			['HP',14]
+		];
+	}
+
+	private function getBooksData()
+	{
+		return [
+			['Children\'s Books',15],
+			['Kindle eBooks',16],
+		];
+	}
+
+	private function getMoviesData()
+	{
+		return [
+			['Family',17],
+			['Romance',18],
+		];
+	}
+
+	private function getRomanceData(): array
+	{
+		return [
+			['Romantic Comedy', 19],
+			['Romantic Drama', 20],
+		];
+	}
+
 }
